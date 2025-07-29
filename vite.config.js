e0ac9c351e3cc6306b5fadace94be52b104de0fe
mcdaoxyz <140123623+mcdaoxyz@users.noreply.github.com>
2025-07-28 19:11:36 -0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import viteCommonjs from 'vite-plugin-commonjs';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-  plugins: [react(), viteCommonjs()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+      protocolImports: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',
+      stream: 'stream-browserify',
+      os: 'os-browserify/browser',
+      path: 'path-browserify',
+    },
+  },
 });
